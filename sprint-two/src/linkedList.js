e7ca5252e6a67648ad8b1fe3d5cee29e29b04d8b
9gramsof <minggui.yactayo@gmail.com>
2022-02-25 15:37:29 -0800

    passed test for linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,74 @@
+/*
+HIGH ORDER
+creates list object containing head/tail, list entries, and methods to modify/search entries
+node function creates list entries with value and reference to next entry
+
+*/
+
+//pseudocode
+
+//create a counter property in list
+
+//addToTail
+//create new node
+//check if head equals null
+//if yes, update head and tail to new node
+//if no, update original tail referenced node to new node
+// and update tail to new node
+
+//removeHead
+//declare removed variable equal to head value
+// reassign head to node's next value
+// return removed
+
+//contains
+// declare truth flag
+// declare recursive search function with node as parameter
+// test if node value matches target
+// if yes, change result to true and return
+// if not, check if next is null
+//invoke recursion starting at head
+//return truth flag
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+
+    var node = new Node(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
   };
 
   list.removeHead = function() {
+    var removed = this.head.value;
+    this.head = this.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    var result = false;
+    let searchNode = function(n) {
+      if (n.value === target) {
+        result = true;
+        return;
+      }
+      if (n.next === null) {
+        return;
+      }
+      searchNode(n.next);
+    };
+    searchNode(this.head);
+
+    return result;
   };
 
   return list;
@@ -26,4 +85,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  //addToTail() - constant
+  //removeHead - constant
+  //contains() - linear
  */
